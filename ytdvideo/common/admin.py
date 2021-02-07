dd5160d2ce576e56df6d27d286d53d9608756c60
gurva <grv1793@gmail.com>
2021-02-07 19:47:06 +0530

    added admin for dashboard view

--- ytdvideo/common/admin.py
+++ ytdvideo/common/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+# from api.admin import EmployerFilter
+# from customer.admin import CustomerFilter
+from api.models import Video
 
-# Register your models here.
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Video._meta.fields]
+    list_per_page = 100
+    fields = list_display
+    readonly_fields = list_display
+    search_fields = ['title', 'description']
+    actions = None
+    list_filter = ('published_at', )
+    date_hierarchy = 'published_at'
+
+    def has_add_permission(self, request):
+        return False
+
+    def change_view(self, request, object_id=None, form_url='',
+                    extra_context=None):
+        template_response = super(VideoAdmin, self).change_view(request, object_id, form_url, extra_context)
+        template_response.content = template_response.rendered_content.replace(
+            '<div class="submit-row">',
+            '<div class="submit-row" style="display: none">')
+        return template_response
+
+    class Media:
+        js = []
+
+admin.site.register(Video, VideoAdmin)
